9bec000ad37a814b46b1ad66aa8bc6f0b07c48db
Koki Yamada <koki.y2764@gmail.com>
2024-05-26 22:52:39 +0900

    Initial Commit

---
+++ exchangeDiary/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  exchangeDiary
+//
+//  Created by 山田航輝 on 2024/05/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
